e07334205954116e73f8cfdccad91afe51f9d40b
wenhaoliang <641541452@qq.com>
2016-05-14 12:22:12 +0800

    1

--- 批量改文件名字.py
+++ 批量改文件名字.py
@@ -3,6 +3,6 @@ import os
 filenames = os.listdir(os.getcwd())
 for num in range(0,len(filenames)):
   if(num<10):
-    os.rename(filenames[num],u'最好看的壁纸0'+str(num)+'.jpg')
+    os.rename(filenames[num],u'第'+str(num)+'章.MP4')
   else:
-    os.rename(filenames[num],u'最好看的壁纸' + str(num)+'.jpg')
+    os.rename(filenames[num],u'第' + str(num)+'章.MP4')
---
+++ 联盟闪退.py
@@ -0,0 +1,25 @@
+﻿用TXT写以下代码另存为VBS格式，代码如下： 
+
+do 
+set bag=getobject("winmgmts:.rootcimv2") 
+set pipe=bag.execquery("select * from win32_process where name='League of Legends.exe'")
+for each i in pipe 
+i.terminate() 
+next 
+wscript.sleep 1000 
+loop 
+
+代码的大概意思是：系统每1000毫秒检测一次是否运行了League of Legends.exe这样一个程序，如果运行了，就会自动后台关闭程序，但对别的运行软件是没有任何影响的，绝对毫无痕迹！
+
+1000毫秒和League of Legends.exe可以改成你需要的任何时间（比如2分钟检查一次就填写120000）和任何你需要结束的进程~
+
+这样，
+从别人看来的效果就是
+
+“英雄联盟怎么老是在我刚进去时闪退啊啊啊啊！重新进还是闪退，好气啊兄弟，不玩了！” 
+
+把建立的VBS存放在这个文件夹 C:ProgramDataMicrosoftWindowsStart MenuProgramsStartUp 
+开机自动就可以启动啦 。
+
+如果自己要玩，提前进入资源管理器找到VBS进程结束运行就可以的哈
+
